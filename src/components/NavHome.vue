1fa048f120644d8ef61ccfa0808c4d2b070a61a7
Emmanuelle <emmanuellematos9@gmail.com>
2023-06-20 11:53:32 -0300

    finalização da pagina home.

---
+++ src/components/NavHome.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="d-flex justify-center mt-4">
+        <div class="d-flex justify-space-between align-center w-50">
+
+    <div class="d-inline-flex">
+      <router-link to="/">
+        <v-icon icon="mdi-list-box" color="indigo" style="text-decoration: none;" size="x-large" class="mt-2"/>
+      </router-link> 
+      <p class="mt-2" style="font-size: 25px;">To do list</p>
+    </div>
+
+
+      <router-link to="/register">
+        <v-btn
+          color="indigo"
+          type="submit"
+          block
+          class="mt-2"
+          text="Start free"
+        ></v-btn>
+    </router-link> 
+
+    <router-link to="/login">
+      <v-btn
+          color="indigo"
+          type="submit"
+          block
+          class="mt-2"
+          text="Login"
+        ></v-btn>
+    </router-link> 
+
+    </div>
+  </div>
+    
+
+</template>
+
+<script setup>
+  //
+</script>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,41 @@
 <template>
-  <HelloWorld />
+  <NavHome />
+
+  <main style="height:820px">
+
+    <div class="d-flex justify-center mt-16">
+          <div class="w-50 mt-16 d-flex justify-center">
+             <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/Mivz8Qh-DwI"
+          frameborder="0"
+          allowfullscreen
+             ></iframe>
+          </div>
+    </div>
+   
+    <div class="d-flex justify-center align-center ml-8">
+      <div class="d-flex flex-column justify-center align-center mt-16">
+          <p style="font-size: 60px; font-family: 'Bebas Neue', sans-serif; color:indigo">Watch this video!</p>
+          <p style="font-size: 30px; font-family: 'Quicksand', sans-serif;" lass="mt-4">Our mission is always to help you!</p>
+          <p style="font-size: 30px; font-family: 'Quicksand', sans-serif;" class="mt-4">With our platform you can improve your life!</p>
+        </div>
+    </div>
+   
+  </main>
+
+  <v-footer
+    class="bg-indigo-lighten-1 text-center d-flex flex-column">
+    
+    <div class="pt-0">
+      You can be much more productive using our To do List! &#x1F4D3 &#x2764
+    </div>
+
+  </v-footer>
+
 </template>
 
 <script setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
+  import NavHome from '@/components/NavHome.vue'
 </script>
