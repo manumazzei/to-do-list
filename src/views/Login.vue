66b2da083bed88cefb526f382a128b5fcc260b57
Emmanuelle <emmanuellematos9@gmail.com>
2023-06-20 13:45:00 -0300

    tentativa deu certo

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -52,11 +52,16 @@
 </script>
 
 <template>
-
-    <Navlogin />
-
+  <div class="w-50 h-75 ml-16">
+    <Navlogin/>
     <h3 class="d-flex justify-center align-center" style="font-size: 20px; color: #5b627d; height: 80px">Welcome Back!</h3>
 
+   
+
+   
+
+<v-sheet max-width="400" class="mx-auto mt-28">
+<v-form v-model="isFormValid" @submit.prevent="submit" style="margin-top: 30px;">
     <v-sheet max-width="400" class="mx-auto mt-30">
       <v-form @submit.prevent="submit" v-model="isFormValid">
         <p>Email</p>
@@ -87,21 +92,48 @@
           class="mt-2"
           text="Sign In"
         ></v-btn>
-        
-        <div class="d-flex justify-center align-center mb-2">
-          <v-divider class="mt-4"></v-divider> <p class="mt-3 pa-2">or</p> <v-divider class="mt-4"></v-divider>
+
+      </v-form>
+    </v-sheet>
+
+        <div class="d-flex justify-center">
+          <div class="d-flex justify-center align-center w-75">
+              <v-divider class="mt-4"></v-divider> <p class="mt-3 pa-2">or</p> <v-divider class="mt-4"></v-divider>
+        </div>
         </div>
 
-        <v-btn variant="outlined" class="d-flex align-center justify-center w-100" @click="showAlert">
+        <div class="d-flex justify-center align-center" style="margin: 20px">
+      <v-btn variant="outlined" class="d-flex align-center justify-center w-75" @click="showAlert">
           <v-avatar>
             <img src="/google-sm.png">
           </v-avatar>
           Continue with Google
         </v-btn>
+    </div>
 
       </v-form>
     </v-sheet>
-  </template>
+    </div>
+ 
+  <div class="m-0" style="position: fixed; top: 0; right: 0; background-color: #e4d5eb; width: 600px;height: 100vh;">
+    <div style="height: 100vh;">
 
-  <style scoped>
-</style>
+     <div style="height: 200px"></div>
+      <div style="width: 600px;" class="d-flex justify-center mt-16">
+      <img src="/dawson-trotman.jpg" class="rounded-circle w-50 h-50">
+      </div>
+
+      <div class="d-inline-flex justify-center align-center">
+        <img src="/aspas.png" style="width: 120px; height: 120px" class="ml-8">
+
+          <h6 style="font-size: 25px; width:350px" class="mt-16">“The greatest amount of wasted time is the time not getting started.”</h6>
+      </div>
+
+      <div class="d-flex justify-center mt-12">
+        <p style="font-size: 20px;">- Dawson Trotman, evangelist</p>
+      </div>
+    </div>
+  </div>
+
+</template>
+
