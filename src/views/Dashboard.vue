3b438823d46b4678266fe1fab0336e4f054f9855
Igor Muller <igormullersantos@hotmail.com>
2023-06-23 11:52:47 -0300

    Formulário de criação de tarefa e atualizando lista de tarefas

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,31 +1,56 @@
 <template>
-    <v-card v-for="list in toDoLists" :key="list.id">
-        <v-card-title>{{ list.title }}</v-card-title>
-    </v-card>
+  <v-form @submit.prevent="createList">
+    <v-text-field
+      :loading="loading"
+      v-model="listTitle"
+      label="Criar nova lista"
+    ></v-text-field>
+    <v-btn :loading="loading">Criar</v-btn>
+  </v-form>
 
+  <v-card v-for="list in toDoLists" :key="list.id">
+    <v-card-title>{{ list.title }}</v-card-title>
+  </v-card>
 </template>
 
 <script>
-import { toDoListsApiMixin } from '@/api/toDoLists';
+import { toDoListsApiMixin } from "@/api/toDoLists";
 export default {
-    mixins: [toDoListsApiMixin],
-    data(){
-        return {
-            toDoLists: []
-        }
+  mixins: [toDoListsApiMixin],
+  data() {
+    return {
+      toDoLists: [],
+      listTitle: "",
+      loading: false,
+    };
+  },
+  methods: {
+    async getLists() {
+      try {
+        const { data } = await this.list();
+        this.toDoLists = data;
+      } catch (err) {
+        alert("Algo deu errado");
+      }
     },
-    methods: {
-        async getLists() {
-            try {
-                const { data } = await this.list ();
-                this.toDoLists = data;
-            } catch (err) {
-                alert("Algo deu errado");
-            }
-        },
+    async createList() {
+      try {
+        this.loading = true;
+        const title = {
+          title: this.listTitle,
+        };
+        const { data } = await this.create(title);
+        this.toDoLists.push(data);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        this.listTitle = "";
+        this.loading = false;
+      }
     },
-    mounted() {
-        this.getLists();
-    },
+  },
+  mounted() {
+    this.getLists();
+  },
 };
 </script>
