435227425d32792c8120da16a51b5f3b9f69ff0e
Emmanuelle <emmanuellematos9@gmail.com>
2023-06-29 10:42:06 -0300

    criação do menu da dashboard.

---
+++ src/components/NavDashboard.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-sheet class="w-100" style="background-color: white; height: 5%; border: 2px solid black">
+        <v-icon icon="mdi mdi-menu-open" color="indigo" style="font-size: 69px;"></v-icon>
+        
+        <v-btn icon="mdi-list-box" color="indigo" style="font-size: 60px;" @click.stop="drawer = !drawer"></v-btn>
+    </v-sheet>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        drawer: null,
+      }
+    },
+  }
+</script>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,49 +1,107 @@
 <template>
-  <v-form @submit.prevent="createList">
-    <v-text-field
-      :loading="loading"
-      v-model="listTitle"
-      label="Criar nova lista"
-    ></v-text-field>
-    <v-btn @click="createList" :loading="loading">Criar</v-btn>
-  </v-form>
+  <v-app>
+    <v-main>
+      <v-layout>
+        <v-app-bar app fixed color="white">
+          <v-btn
+            icon
+            @click="rail = !rail"
+          >
+            <v-icon color="indigo" style="font-size: 34px;">mdi-menu-open</v-icon>
+          </v-btn>
 
-  <v-card v-for="list in toDoLists" :key="list.id">
-    <v-list-item>
-      <router-link :to="`/list-detail/${list.id}`">
-        <v-card-title>{{ list.title }}</v-card-title>
-      </router-link>
+          <v-sheet class="d-inline-flex ml-6">
+            <router-link to="/">
+              <v-icon icon="mdi-list-box" color="indigo" style="text-decoration: none;" size="x-large" class="mt-2"/>
+            </router-link> 
+            <strong class="mt-2 ml-2" style="font-size: 25px;">To do list</strong>
+          </v-sheet>
 
-      <template v-slot:append>
-        <v-icon @click="startRemove(list.id)" color="error">
-          mdi-delete
-        </v-icon>
-      </template>
-    </v-list-item>
-  </v-card>
+          <v-sheet class="ml-4" style="width:75%">
+          
+          </v-sheet>
 
-  <v-card v-show="showRemove">
-    <v-card-title class="font-weight-bold text-h5 text-error"
-      >Deletar</v-card-title
-    >
-    <v-card-text>Tem certeza que deseja excluir?</v-card-text>
+          <v-sheet class="d-flex justify-space-between ml-4" style="width: 10%">
+            <v-icon class="mdi mdi-help-circle-outline mt-2"></v-icon>
+            <v-icon class="mdi mdi-bell-outline mt-2"></v-icon>
+            <v-avatar>
+              <v-img
+              src="/horacio-poeta.jpg"
+              contain
+              class="rounded-circle"
+              >
+              </v-img>
+            </v-avatar>
+          </v-sheet>
 
-    <v-card-actions>
-      <v-btn
-        :loading="removeLoad"
-        @click="removeList(this.selected)"
-        color="error"
-        >Deletar</v-btn
-      >
-      <v-btn @click="showRemove = !showRemove">Cancelar</v-btn>
-    </v-card-actions>
-  </v-card>
+        </v-app-bar>
+
+        <v-navigation-drawer
+          :rail="rail"
+          permanent
+        >
+          <v-list density="compact" nav>
+              <v-list-item prepend-icon="mdi-home-city" title="Home" value="home"></v-list-item>
+              <v-list-item prepend-icon="mdi-account" title="My Account" value="account"></v-list-item>
+              <v-list-item prepend-icon="mdi-account-group-outline" title="Users" value="users"></v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+
+        <v-main style="height: 1000px; background-color: whitesmoke;">
+          <v-form @submit.prevent="createList">
+            <v-text-field
+              :loading="loading"
+              v-model="listTitle"
+              label="Criar nova lista"
+            ></v-text-field>
+            <v-btn @click="createList" :loading="loading">Criar</v-btn>
+          </v-form>
+
+          <v-card v-for="list in toDoLists" :key="list.id">
+            <v-list-item>
+              <router-link :to="`/list-detail/${list.id}`">
+                <v-icon icon="mdi-checkbox-marked-circle-outline" color="indigo"></v-icon>
+              </router-link>
+              <v-card-title>{{ list.title }}</v-card-title>
+
+              <template v-slot:append>
+                <v-icon @click="startRemove(list.id)" color="error">
+                  mdi-delete
+                </v-icon>
+              </template>
+            </v-list-item>
+          </v-card>
+
+          <v-card v-show="showRemove">
+            <v-card-title class="font-weight-bold text-h5 text-error">Deletar</v-card-title>
+            <v-card-text>Tem certeza que deseja excluir?</v-card-text>
+
+            <v-card-actions>
+              <v-btn
+                :loading="removeLoad"
+                @click="removeList(this.selected)"
+                color="error"
+              >Deletar</v-btn>
+              <v-btn @click="showRemove = !showRemove">Cancelar</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-main>
+      </v-layout>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
 import { toDoListsApiMixin } from "@/api/toDoLists";
+import NavDashboard from "@/components/NavDashboard.vue";
+import MenuDashboard from "@/components/MenuDashboard.vue";
+
 export default {
   mixins: [toDoListsApiMixin],
+  components: {
+    NavDashboard,
+    MenuDashboard
+  },
   data() {
     return {
       toDoLists: [],
@@ -52,6 +110,7 @@ export default {
       showRemove: false,
       selected: "",
       removeLoad: false,
+      rail: true
     };
   },
   methods: {
